023aac2f206cc8694a1cc0bbbfc831de40c3cf66
Fabien Baron <baron@chara.gsu.edu>
2017-04-26 11:09:01 -0400

    Fixed typo that prevented compilation

--- src/Lbfgsb.jl
+++ src/Lbfgsb.jl
@@ -1,10 +1,5 @@
 module Lbfgsb
 
-if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
-    include("../deps/deps.jl")
-else
-    error("Lbfgsb is not properly installed. Please run Pkg.build(\"Lbfgsb\")")
-end
 
 # package code goes here
 macro callLBFGS(cmd)
@@ -18,7 +13,7 @@ macro callLBFGS(cmd)
             end
         end
 
-        ccall((:setulb_, liblbfgsbf),
+        ccall((:setulb_, "/home/baron/.julia/v0.5/Lbfgsb/deps/usr/lib/liblbfgsbf.so"),
               Void,
               (Ptr{Int32},
                Ptr{Int32},
@@ -32,9 +27,9 @@ macro callLBFGS(cmd)
                Ptr{Float64},
                Ptr{Float64},
                Ptr{Int32},
-               Ptr{Uint8},
+               Ptr{UInt8},
                Ptr{Int32},
-               Ptr{Uint8},
+               Ptr{UInt8},
                Ptr{Bool},
                Ptr{Int32},
                Ptr{Float64} ),
@@ -59,8 +54,8 @@ macro callLBFGS(cmd)
     end
 end
 
-    
-function lbfgsb (ogFunc!::Function,
+
+function lbfgsb(ogFunc!::Function,
                  x::Array;
                  lb = [],
                  ub = [],
@@ -72,12 +67,12 @@ function lbfgsb (ogFunc!::Function,
                  iprint::Int64 = -1 # does not print
                  )
     initial_x = x;
-        
+
     m = [convert(Int32, m)]
     factr = [convert(Float64, factr)];
     pgtol = [convert(Float64, pgtol)];
     iprint = [convert(Int32, iprint)];
-    
+
     n = [convert(Int32, length(x))]; # number of variables
     f = [convert(Float64, 0.0)]; # The value of the objective.
     g = [convert(Float64, 0.0) for i=1:(n[1])]; # The value of the gradient.
@@ -103,8 +98,8 @@ function lbfgsb (ogFunc!::Function,
     # structures used by the L-BFGS-B routine.
     wa = [convert(Float64, 0.0) for i = 1:(2*m[1] + 5)*n[1] + 12*m[1]*(m[1] + 1)];
     iwa = [convert(Int32, 0) for i = 1:3*n[1]];
-    task = [convert(Uint8, 0) for i =1:60];
-    csave = [convert(Uint8, 0) for i =1:60];
+    task = [convert(UInt8, 0) for i =1:60];
+    csave = [convert(UInt8, 0) for i =1:60];
     lsave = [convert(Bool, 0) for i=1:4];
     isave = [convert(Int32, 0) for i=1:44];
     dsave = [convert(Float64, 0.0) for i=1:29];
@@ -118,22 +113,22 @@ function lbfgsb (ogFunc!::Function,
 
     while true
 
-        if task[1] == 'F'
+        if task[1] == UInt32('F')
             f[1] = convert( Float64, ogFunc!(x,g) );
             c += 1;
-            
-        elseif task[1] == 'N'
+
+        elseif task[1] == UInt32('N')
             t += 1;
             if t >= maxiter # exceed maximum number of iteration
                 @callLBFGS "STOP"
                 break;
             end
-        elseif task[1] == 'C' # convergence
+        elseif task[1] == UInt32('C') # convergence
             break;
-        elseif task[1] == 'A'
+        elseif task[1] == UInt32('A')
             status = "abnormal";
             break;
-        elseif task[1] == 'E'
+        elseif task[1] == UInt32('E')
             status = "error";
             break;
         end
@@ -142,11 +137,11 @@ function lbfgsb (ogFunc!::Function,
     end
 
     return (f[1], x, t, c, status)
-    
+
 end # function lbfgsb
 
 
-function lbfgsb (objFunc::Function,
+function lbfgsb(objFunc::Function,
                  gradFunc!::Function,
                  x::Array;
                  lb = [],
@@ -158,14 +153,14 @@ function lbfgsb (objFunc::Function,
                  pgtol::Float64 = 1e-5,
                  iprint::Int64 = -1 # does not print
                  )
-    
+
     function _ogFunc!(x, g::Array)
         gradFunc!(x, g);
         return objFunc(x);
     end
-    
+
     return lbfgsb(_ogFunc!,
-           x;
+           x,
            lb=lb,
            ub=ub,
            btype=btype,
@@ -176,10 +171,6 @@ function lbfgsb (objFunc::Function,
            iprint=iprint # does not print
            )
 end # function lbfgsb
-
-
-
-
 
 export lbfgsb
 
